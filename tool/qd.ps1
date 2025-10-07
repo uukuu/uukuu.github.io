fa49e08531883b0a3390007d44dcade8967a061b
uuku <10873494+uuku_lxn@user.noreply.gitee.com>
2025-10-07 12:39:04 +0800

    Auto commit before deploy

---
+++ tool/qd.ps1
@@ -0,0 +1,3 @@
+FreeFileSync.exe E:\University\Blog\tool\studyfigure.ffs_batch
+FreeFileSync.exe E:\University\Blog\tool\blog.ffs_batch
+python E:\University\Blog\tool\deploy_github_pages.py --push --incremental
